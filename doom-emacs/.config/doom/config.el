0d32e29c8ffb310cc53145905f973795daa42065
Galletta Paolo <paolo.galletta@saipem.com>
2025-03-02 15:22:49 +0100

    Applied additional changes in the config

--- doom-emacs/.config/doom/config.el
+++ doom-emacs/.config/doom/config.el
@@ -12,9 +12,27 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-nord)
 
-(setq org-directory "~/Documents/PKDB_Org/Notes")
+(when (string= "windows-nt" system-type)
+  ;; Configuration for windows
+  (setq org-directory "~/Desktop/PKDB_Org/Notes")
+  )
 
-(setq org-roam-directory "~/Documents/PKDB_Org/Zettlekasten")
+(when (string= "gnu/linux" system-type)
+  ;; Configuration for Linux
+
+  (setq org-directory "~/Documents/PKDB_Org/Notes")
+  )
+
+(when (string= "windows-nt" system-type)
+  ;; Configuration for windows
+  (setq org-roam-directory "~/Desktop/PKDB_Org/Zettlekasten/")
+  )
+
+(when (string= "gnu/linux" system-type)
+  ;; Configuration for Linux
+
+  (setq org-roam-directory "~/Documents/PKDB_Org/Zettlekasten/")
+  )
 
 ;
 ; Setup org-roam-ui
@@ -29,15 +47,83 @@
         org-roam-ui-update-on-save t
         org-roam-uiopen-on-start t))
 
+;; Bind this to Spc n r I
+(defun org-roam-node-insert-immediate (arg &rest args)
+  (interactive "P")
+  (let ((args (cons arg args))
+        (org-roam-capture-templates (list (append (car org-roam-capture-templates)
+                                                  '(:immediate-finish t)))))
+    (apply #'org-roam-node-insert args)))
+
+(add-hook 'org-roam-file-setup-hook #'glz/update-todo-tag)
+(add-hook 'before-save-hook #'glz/update-todo-tag)
+(advice-add 'org-agenda :before #'glz/update-todo-files)
+(defun glz/todo-p ()
+  "Return non-nil if current buffer has any TODO entry.
+
+TODO entries marked as done are ignored, meaning the this
+function returns nil if current buffer contains only completed
+tasks."
+  (org-element-map
+      (org-element-parse-buffer 'headline)
+      'headline
+    (lambda (h)
+      (eq (org-element-property :todo-type h)
+          'todo))
+    nil 'first-match))
+
+(defun glz/update-todo-tag ()
+  "Update TODO tag in the current buffer."
+  (when (and (not (active-minibuffer-window))
+             (org-roam--org-file-p buffer-file-name))
+    (let* ((file (buffer-file-name (buffer-base-buffer)))
+           (all-tags (org-roam--extract-tags file))
+           (prop-tags (org-roam--extract-tags-prop file))
+           (tags prop-tags))
+      (if (glz/todo-p)
+          (setq tags (seq-uniq (cons "todo" tags)))
+        (setq tags (remove "todo" tags)))
+      (unless (equal prop-tags tags)
+        (org-roam--set-global-prop
+         "roam_tags"
+         (combine-and-quote-strings tags))))))
+
+(defun glz/todo-files ()
+  "Return a list of note files containing todo tag."
+  (seq-map
+   #'car
+   (org-roam-db-query
+    [:select file
+             :from tags
+             :where (like tags (quote "%\"todo\"%"))])))
+
+(defun glz/update-todo-files (&rest _)
+  "Update the value of `org-agenda-files'."
+  (setq org-agenda-files (glz/todo-files)))
+
+(dolist (file (org-roam-list-files))
+  (message "processing %s" file)
+  (with-current-buffer (or (find-buffer-visiting file)
+                           (find-file-noselect file))
+    (glz/update-todo-files)
+    (save-buffer)))
+
 ;; Automatically tangle our Emacs.org config file when we save it
 (defun glz/org-babel-tangle-config ()
-  (when (string-equal (buffer-file-name)
-                      (expand-file-name "~/.config/doom/config.org"))
+  (when (string-equal (file-name-nondirectory buffer-file-name)
+                      ; Select different location depending on the os (hopefully this works in elisp :D)
+                      "config.org")
 
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'glz/org-babel-tangle-config)))
 
+(map! :after org-roam
+      :map org-roam-immediate-insert
+      "SPC n r I" #'org-roam-insert-immediate)
+
 (setq shell-file-name (executable-find
       "bash"))
+
+(setq default-directory "~")
