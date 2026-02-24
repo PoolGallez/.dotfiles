13f96acc16da2e46beca4bf2df01082b12b15e3c
Paolo Galletta <galletta.paolo98@gmail.com>
2026-02-25 00:00:34 +0100

    Initial almost usable version of emacs configuration written from scratch, thanks to system crafters

---
+++ emacs/init.el
@@ -0,0 +1,163 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)            ; Disable the menu bar
+
+
+(column-number-mode 1)         ;; Show column number on mode line
+
+;; Tabs to spaces
+(setq-default indent-tabs-mode nil
+	      tab-width 2)
+
+;; Display line numbers in programming modes
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+;; Display relative line numbers
+(setq display-line-numbers 'relative)
+
+
+; (set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
+
+
+;; Make ESC quit prompts instead of counting as chord
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package) ; Package manager
+(setq use-package-always-ensure t)
+
+; Set of tools for better completion on emacs
+(use-package ivy
+  :diminish                       ; Prevents to appear on the linemode
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(global-set-key (kbd "C-M-j") 'counsel-switch-buffer)
+
+
+;; Better completion with more details (function descriptions, ...)
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; Better Emacs function search (fuzzy search, ...)
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ; Remove the starting^
+
+(use-package swiper)
+
+
+(use-package all-the-icons)
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))`
+
+(use-package doom-themes
+  :config
+  (load-theme 'doom-dracula t))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Color parenthesis
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode)) 
+
+;; Helper for the key bindings
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config (setq which-key-idle-delay 0.3))
+
+
+
+;; Better help screen
+(use-package helpful
+  :custom                        ; Set variable of customizable variables
+  (counsel-describe-function-function #'helpful-callable)    ; set counsel help variables to helpful
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)    ; remap exiting keys to new function
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; General for unified way of defining keymaps (with additional features)
+(use-package general
+  :config
+  (general-evil-setup t))
+
+;; Going evil
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  ;:hook (evil-mode . rune/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+;; For the parts of emacs not covered by Evil already
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
