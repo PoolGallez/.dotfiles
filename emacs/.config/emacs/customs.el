b924df0c46e11197f4fa9fd6a46c63d6d981e8f9
Paolo Galletta <galletta.paolo98@gmail.com>
2026-03-20 18:36:16 +0100

    Migrated to Vertico, consult, marginalia, .. adjusted keymaps

--- emacs/.config/emacs/customs.el
+++ emacs/.config/emacs/customs.el
@@ -1,16 +1,14 @@
-  
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons counsel counsel-projectile doom-modeline doom-themes
-                   evil-collection evil-magit forge general helpful
-                   ivy-rich magit no-littering org-bullets projectile
-                   rainbow-delimiters visual-fill-column)))
+   '(all-the-icons command-log-mode consult doom-modeline doom-themes
+                   evil-collection forge general helpful marginalia
+                   no-littering orderless org-bullets
+                   rainbow-delimiters vertico visual-fill-column)))
 
-  
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,13 +1,13 @@
 (setq gc-cons-threshold (* 50 1000 1000))
 
-(defun efs/display-startup-time ()
+(defun glz/display-startup-time ()
   (message "Emacs loaded in %s with %d garbage collections."
            (format "%.2f seconds"
                    (float-time
                    (time-subtract after-init-time before-init-time)))
            gcs-done))
 
-(add-hook 'emacs-startup-hook #'efs/display-startup-time)
+(add-hook 'emacs-startup-hook #'glz/display-startup-time)
 
 (setq inhibit-startup-message t)
   (scroll-bar-mode -1)        ; Disable visible scrollbar
@@ -29,8 +29,7 @@
   "Personal adjustments for programming modes"
   ; Display line numbers
   (display-line-numbers-mode)
-  ; Set them as relative numbers
-  (setq display-line-numbers 'relative))
+)
 
 ;; Add hook to all programming mode to satisfy my customizations
 (add-hook 'prog-mode-hook #'glz/prog-mode-configs)
@@ -51,16 +50,18 @@
     :global-prefix "C-SPC")
   (glz/leader-key
     ; Fast shortcuts
-    "." '("Find file in current directory" . counsel-find-file)
+    "." '("Find file in current directory" . find-file)
     ; Git group
     "g" '(:ignore t :which-key "git"); how to define groups of keymaps, not bound directly to commands
     "gg" '("Open Magit" . magit-status)
     ; Projectile group
-    "p" '(:package projectile :keymap projectile-command-map :which-key "projectile") ; How to assign a prefix to existin map      ; Buffers group
+    "p" '(:package project :keymap project-prefix-map :which-key "project") ; How to assign a prefix to existin map      ; Buffers group
+    "pp" '("Switch projects" . project-switch-project)
     "b" '(:ignore t :which-key "buffers")
-    "bb" '("Switch buffers" . counsel-switch-buffer)
+    "bb" '("Switch buffers" . consult-buffer)
     "h" '(:package help :keymap help-map :which-key "help") ; How to assign a prefix to existin map      ; Buffers group
     "c" '("Open Config" lambda () (interactive) (find-file (expand-file-name (concat user-emacs-directory "Config.org"))))
+    "s" '("Open eshell" . eshell)
     ))
 
 ;; Initialize package sources
@@ -85,40 +86,218 @@
   (load-theme 'doom-dracula t))
 
 ; Set of tools for better completion on emacs
-(use-package ivy
-  :diminish                       ; Prevents to appear on the linemode
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
+;; (use-package ivy
+;;   :diminish                       ; Prevents to appear on the linemode
+;;   :bind (("C-s" . swiper)
+;;          :map ivy-minibuffer-map
+;;          ("TAB" . ivy-alt-done)	
+;;          ("C-l" . ivy-alt-done)
+;;          ("C-j" . ivy-next-line)
+;;          ("C-k" . ivy-previous-line)
+;;          :map ivy-switch-buffer-map
+;;          ("C-k" . ivy-previous-line)
+;;          ("C-l" . ivy-done)
+;;          ("C-d" . ivy-switch-buffer-kill)
+;;          :map ivy-reverse-i-search-map
+;;          ("C-k" . ivy-previous-line)
+;;          ("C-d" . ivy-reverse-i-search-kill))
+;;   :config
+;;   (ivy-mode 1))
 
 ;; Better Emacs function search (fuzzy search, ...)
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil)) ; Remove the starting^
+;; (use-package counsel
+;;   :bind (("M-x" . counsel-M-x)
+;;          ("C-x b" . counsel-ibuffer)
+;;          ("C-x C-f" . counsel-find-file)
+;;          :map minibuffer-local-map
+;;          ("C-r" . 'counsel-minibuffer-history))
+;;   :config
+;;   (setq ivy-initial-inputs-alist nil)) ; Remove the starting^
 
-(use-package swiper)
+;  (use-package swiper)
 
 ;; Better completion with more details (function descriptions, ...)
-(use-package ivy-rich
+;; (use-package ivy-rich
+;;   :init
+;;   (
+;;     ivy-rich-mode 1))
+
+(use-package vertico
+  :after evil
+  :custom
+  (vertico-scroll-margin 0) ;; Different scroll margin
+  (vertico-count 20) ;; Show more candidates
+  (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+  (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :bind (:map vertico-map
+              ; Use page-up/down to scroll vertico buffer, like ivy does by default.
+              ("C-j" . 'vertico-next)
+              ("C-M-j" . #'vertico-next-group)
+              ("C-k" . #'vertico-previous)
+              ("C-M-k" . #'vertico-previous-group))
   :init
-  (ivy-rich-mode 1))
+  (vertico-mode t))
+
+;; Enable rich annotations using the Marginalia package
+(use-package marginalia
+  ;; Bind `marginalia-cycle' locally in the minibuffer.  To make the binding
+  ;; available in the *Completions* buffer, add it to the
+  ;; `completion-list-mode-map'.
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init section is always executed.
+  :init
+
+  ;; Marginalia must be activated in the :init section of use-package such that
+  ;; the mode gets enabled right away. Note that this forces loading the
+  ;; package.
+  (marginalia-mode))
+
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :custom
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch))
+  ;; (orderless-component-separator #'orderless-escapable-split-on-space)
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-category-defaults nil) ;; Disable defaults, use our settings
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+
+(use-package consult
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g r" . consult-grep-match)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("C-s" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Tweak the register preview for `consult-register-load',
+  ;; `consult-register-store' and the built-in commands.  This improves the
+  ;; register formatting, adds thin separator lines, register sorting and hides
+  ;; the window mode line.
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult-source-bookmark consult-source-file-register
+   consult-source-recent-file consult-source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+  
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+)
+
+;;   (use-package embark
+;;   :ensure t
+
+;;   :bind
+;;   (("C-." . embark-act)         ;; pick some comfortable binding
+;;    ("C-;" . embark-dwim)        ;; good alternative: M-.
+;;    ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+;;   :init
+;;   ;; Optionally replace the key help with a completing-read interface
+;;   (setq prefix-help-command #'embark-prefix-help-command)
+
+;;   ;; Show the Embark target at point via Eldoc. You may adjust the
+;;   ;; Eldoc strategy, if want to see the documentation from
+;;   ;; multiple providers. Beware that using this can be a little
+;;   ;; jarring since the message shown in the minibuffer can be more
+;;   ;; than one line, causing the modeline to move up and down:
+
+;;   ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+;;   ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+;;   ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
+;;   ;; (context-menu-mode 1)
+;;   ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
+
+;;   :config
+
+;;   ;; Hide the mode line of the Embark live/completions buffers
+;;   (add-to-list 'display-buffer-alist
+;;                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+;;                  nil
+;;                  (window-parameters (mode-line-format . none)))))
+;; ;; Consult users will also want the embark-consult package.
+;; (use-package embark-consult
+;;   :ensure t) ; only need to install it, embark loads it after consult if found
 
 (use-package all-the-icons)
 (use-package doom-modeline
@@ -136,13 +315,13 @@
 
 ;; Better help screen
 (use-package helpful
-  :custom                        ; Set variable of customizable variables
-  (counsel-describe-function-function #'helpful-callable)    ; set counsel help variables to helpful
-  (counsel-describe-variable-function #'helpful-variable)
+  ;; :custom                        ; Set variable of customizable variables
+  ;; (counsel-describe-function-function #'helpful-callable)    ; set counsel help variables to helpful
+  ;; (counsel-describe-variable-function #'helpful-variable)
   :bind
-  ([remap describe-function] . counsel-describe-function)    ; remap exiting keys to new function
+  ([remap describe-function] .  helpful-callable)   ; remap exiting keys to new function
   ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-variable] . helpful-variable)
   ([remap describe-key] . helpful-key))
 
 ;; Color parenthesis
@@ -173,31 +352,8 @@
 (use-package evil-collection
   :after evil
   :config
-  (evil-collection-init))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons counsel counsel-projectile doom-modeline doom-themes
-                   evil-collection evil-magit forge general helpful
-                   ivy-rich magit org-bullets projectile
-                   rainbow-delimiters visual-fill-column)))
-
-;; Project tile for managing projects 
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy)) ; Use ivy also in projectile
-  ;:bind-keymap
-  ;("C-c p" . projectile-command-map)
+  (evil-collection-init)
   )
-;; Integrate the better completion framework counsel and ivy into projectile
-(use-package counsel-projectile
- :after projectile
- :config
- (counsel-projectile-mode 1))
 
 ;; Magit git client
 (use-package magit
@@ -212,17 +368,17 @@
 (setq ediff-make-buffers-readonly-at-startup nil)
 (setq ediff-merge-revisions-with-ancestor t)
 (setq ediff-show-clashes-only t)
-
 (setq ediff-split-window-function 'split-window-horizontally)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
 ;; Automatically tangle our Emacs.org config file when we save it
 (defun glz/org-babel-tangle-config ()
-  (when (string-equal (buffer-file-name)
-                      (expand-file-name "~/.dotfiles/emacs/.config/emacs/Config.org"))
+  (when (string-search "Config.org" (buffer-file-name))
+                      
     ;; Dynamic scoping to the rescue
     (let ((org-confirm-babel-evaluate nil))
       (org-babel-tangle))))
+
 
 (add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'glz/org-babel-tangle-config)))
 
@@ -290,7 +446,7 @@
 (add-to-list 'org-structure-template-alist '("py" . "src python"))
 
 ;; Org notes directories
-(setq org-directory "/home/pool/Documents/PKDB_Org/") 
+(setq org-directory "~/Documents/PKDB_Org/") 
 
  ;; Org agenda 
 (setq org-agenda-files '("Tasks.org" "Birthdays.org" "Calendar.org"))
