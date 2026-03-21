1907de54426bda303c0192758c192d9977a69697
Paolo Galletta <galletta.paolo98@gmail.com>
2026-03-21 14:17:19 +0100

    Y or n alias, multi cursor, toggles, ...

--- emacs/.config/emacs/customs.el
+++ emacs/.config/emacs/customs.el
@@ -3,12 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dired-listing-switches "-al --group-directories-first")
+ '(display-line-numbers-type 'relative)
  '(package-selected-packages
    '(all-the-icons all-the-icons-dired command-log-mode consult dired
-                   doom-modeline doom-themes evil-collection forge
-                   general helpful marginalia no-littering orderless
-                   org-bullets org-roam org-roam-ui rainbow-delimiters
-                   vertico visual-fill-column)))
+                   doom-modeline doom-themes embark embark-consult
+                   evil-collection evil-mc forge general helpful
+                   marginalia no-littering orderless org-bullets
+                   org-roam org-roam-ui rainbow-delimiters vertico
+                   visual-fill-column))
+ '(project-list-file "/home/pool/.config/emacs/var/project-list.el"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
