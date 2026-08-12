52885777415babee322154fa734c0c8a0275b779
Paolo Galletta <galletta.paolo98@gmail.com>
2026-08-12 12:57:29 +0200

    Update with meow-edit and removed unused packages

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -4,10 +4,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(dired-listing-switches "-al --group-directories-first")
- '(display-line-numbers-type 'relative)
- '(evil-undo-system 'undo-redo)
- '(org-roam-db-autosync-mode t)
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
